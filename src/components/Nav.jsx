726b6dcba37a8fbc3968b07ce879aed43641e422
Jorge Matos <jisapjisap@gmail.com>
2024-08-25 23:28:03 +0200

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { navData } from '../data'
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className='w-full h-full'>
+      <ul className='h-full flex flex-col justify-center items-center gap-y-6'>
+        {navData.map((item, index) => {
+          return (
+            <li key={index}>
+              <a 
+                href={item.href}
+                className='text-xl capitalize font-primary font-semibold italic hover:text-dark transition-all duration-300'
+              >
+                {item.name}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
 };
 
 export default Nav;
