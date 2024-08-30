037daaaabfe9e37dbdccfd42dc3b01e0bacd3d5e
Jorge Matos <jisapjisap@gmail.com>
2024-08-30 19:50:41 +0200

    newsletter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Newsletter from './Newsletter';
+import Copyright from './Copyright';
+import Socials from './Socials';
+import { footerData} from '../data'
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from '../variants';
+
 
 const Footer = () => {
+  
+  const { contact, hours, social} = footerData
+  
   return (
-    <div>Footer</div>
+    <footer className='relative top-96 z-20 bg-dark lg:bg-transparent lg:min-h-[620px] lg:bg-footer lg:bg-center lg:bg-no-repeat pt-20'>
+      <div className='container mx-auto h-full'>
+        {/* newsletter & info */}
+        <div>
+          {/* newsletter */}
+          <Newsletter />
+          {/* info */}
+          <div>info</div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
