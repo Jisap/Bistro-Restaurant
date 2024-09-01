b750dc36106a82d5e51da41b259f54e83a9132bb
Jorge Matos <jisapjisap@gmail.com>
2024-09-01 12:18:16 +0200

    finish footer

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -7,8 +7,7 @@ const Socials = () => {
       {socialData.map((item, index) => {
         return (
           <a
-            className='border border-white/20 rounded-full w-[35px] h-[35px] flex items-center justify-center 
-            text-sm hover:text-accent hover:border-accent transition-all'
+            className='border border-white/20 rounded-full w-[35px] h-[35px] flex items-center justify-center text-sm hover:text-accent hover:border-accent transition-all'
             key={index}
             href={item.href} 
           >
